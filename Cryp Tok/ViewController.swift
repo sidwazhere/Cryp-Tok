5394b42edfd974a1b074d6ed4028e91feae26afe
Sideeq <sideeqw@gmail.com>
2021-11-10 11:24:24 -0500

    Initial Commit

--- Cryp Tok/ViewController.swift
+++ Cryp Tok/ViewController.swift
@@ -8,11 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var LiveMarket: UILabel!
+    
+    
+    @IBOutlet weak var investmentAmount: UILabel!
+    
+    @IBOutlet weak var bitCoinPrice: UILabel!
+    
+    @IBOutlet weak var EtherPrice: UILabel!
+    
+    
+    @IBOutlet weak var DogePrice: UILabel!
+    
+    @IBOutlet weak var ShibaPrice: UILabel!
+    
+    
+    @IBAction func uiStock(_ sender: UIButton) {
+    }
+    @IBAction func uiCrypto(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
